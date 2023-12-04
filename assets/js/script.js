31ec7d3f71216c464510bde50803c864bd4c3373
Liene Briede <lienesjaunkundze@gmail.com>
2023-12-04 06:48:03 +0000

    Create js file, add questions array

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const questions = [
+    {
+        question: "Super Mario is the mascot of which video game developer?",
+        answers: ["Nintendo", "TenCent", "Sony", "Sega"],
+        correctAnswer: "Nintendo",
+        comment: "It's Nintendo! Did you know that Nintendo originally sold playing cards? Let's-a-go!"
+    },
+    {
+        question: "Where does boba come from?",
+        answers: ["China", "Japan", "South Korea", "Taiwan"],
+        correctAnswer: "Taiwan",
+        comment: "It's Taiwan! The Taiwanese have been enjoying it since 1981!"
+    },
+    {
+        question: "Which popular K-pop boy band had the hit singles “Butter” and “Dynamite”?",
+        answers: ["Epic High", "The BoyZ", "BTS", "Exo"],
+        correctAnswer: "BTS",
+        comment: "It's BTS! Jungkook, V, Jimin, SUGA, Jin, RM, j-hope FTW."
+    },
+    {
+        question: "In which language would you say “Arigatou” to thank somebody?",
+        answers: ["Korean", "Cantonese", "Japanese", "Mandarin"],
+        correctAnswer: "Japanese",
+        comment: "It's Japanese! But if you forget how to say it, just bow!"
+    }
+];
